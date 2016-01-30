dcc08840ea0968b6c5593eba7e2cd2f2d0f04592
Vishay Nihalani <vnihalani@lyft.com>
2016-01-30 04:50:50 -0800

    animations
    it took me forever, but it fucking works

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,44 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+
+    @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var amountForOneLabel: UILabel!
+    @IBOutlet weak var amountForTwoLabel: UILabel!
+    @IBOutlet weak var amountForThreeLabel: UILabel!
+    @IBOutlet weak var amountForFourLabel: UILabel!
     
+    var initialPosTopView: CGFloat!
+    var initialPosBottomView: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
+        initialPosTopView = topView.center.y
+        initialPosBottomView = bottomView.center.y
         
         billAmountTextField.becomeFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func onEditingChanged(sender: AnyObject) {
+            
+        if (billAmountTextField.text == "") {
+
+            UIView.animateWithDuration(0.5, animations: {
+                self.topView.center.y = self.initialPosTopView
+                self.tipControl.alpha = 0
+                
+                self.bottomView.center.y = self.initialPosBottomView
+                self.bottomView.alpha = 0
+            })
+        } else {
+            
+            UIView.animateWithDuration(0.5, animations: {
+                self.topView.center.y = self.initialPosTopView - 145
+                self.tipControl.alpha = 1
+                
+                self.bottomView.center.y = self.initialPosBottomView - 145
+                self.bottomView.alpha = 1
+            })
+        }
         
-        
-        var billAmount = NSString(string: billAmountTextField.text!).doubleValue
+        self.updateLabels()
+    }
+    
+    func updateLabels() {
         
         var tipPercentages = [0.18, 0.20, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
+        var billAmount = NSString(string: billAmountTextField.text!).doubleValue
+        
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+
+        // Commenting out the following 4 lines makes the animation work, but prevents the label from updating
+        tipLabel.text = "$/(tip)"
+        amountForOneLabel.text = "$/(total)"
         
+        tipLabel.text = String(format: "$%.2f", tip)
+        amountForOneLabel.text = String(format: "$%.2f", total)
+        amountForTwoLabel.text = String(format: "$%.2f", total/2.0)
+        amountForThreeLabel.text = String(format: "$%.2f", total/3.0)
+        amountForFourLabel.text = String(format: "$%.2f", total/4.0)
     }
     
     @IBAction func onTap(sender: AnyObject) {
         
         billAmountTextField.resignFirstResponder()
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
 
